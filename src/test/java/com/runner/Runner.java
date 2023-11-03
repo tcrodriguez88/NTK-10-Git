1a4b0359382e64d9d51e0e85fc24b6b6b0214524
noort <noort@NoorTeck>
2023-11-03 16:51:19 -0400

    initial

---
+++ src/test/java/com/runner/Runner.java
@@ -0,0 +1,28 @@
+package com.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+
+
+
+
+@CucumberOptions (
+					features = "src/test/resources/smoke/SmokeTest.feature",
+					glue = "com.steps"		
+				)
+
+public class Runner {
+
+}
+
+
+/**
+	in this Runner class we are providing specific feature file to execute
+
+
+*/
